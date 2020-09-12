2f965dc3b7a845e7b06d64ced2ec993f95c42f38
ops <dmitriycr3w@gmail.com>
2020-09-12 15:50:11 +0300

    added students app

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -32,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'courses',
+    'students',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
 
 #TODO
 """ 343 """
